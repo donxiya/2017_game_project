cbfa98bd024cbf00b07b1d5afa13ed94ddd604a9
donxiya <31514669+donxiya@users.noreply.github.com>
2017-09-26 22:45:40 -0500

    Button animation

---
+++ Thicker Than Blood/Assets/GameScene/MenuScene/Script/ButtonColorAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonColorAnimation : MonoBehaviour {
+    public Animator animator;
+    private void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+    public void pointerEnter()
+    {
+        animator.SetBool("hover", true);
+    }
+    public void pointerExit()
+    {
+        animator.SetBool("hover", false);
+    }
+}
